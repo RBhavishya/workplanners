ca5957a7968ddb71290b84b9a26842d562db8f99
code@labsquire <code@labsquire.com>
2025-09-10 09:36:56 +0530

    hc--add a aceses token in projects

--- src/components/Taskfiles/Tasks.tsx
+++ src/components/Taskfiles/Tasks.tsx
@@ -8,9 +8,10 @@ import {
   FileClock,
 } from "lucide-react";
 import { Button } from "@/components/ui/button";
+
 import BigCard from "../core/Cards";
+import AddTaskForm from "./AddTaskForm";
 import SmallCard from "../core/StatusCard";
-import AddTaskForm from "./AddTask";
 
 
 const Tasks = () => {
@@ -34,7 +35,10 @@ const Tasks = () => {
     <div className="p-4">
       <div className="flex items-center mb-6 w-full">
         <div className="flex w-3/4 justify-center rounded gap-4">
-          <SmallCard cards={[]} />
+          <SmallCard cards={[{ title: "TODAY", value:0},
+    { title: "OVERDUE", value: 0},
+    { title: "CLOSED", value: 0},
+  ]} />
         </div>
         {/* divider */}
         <div className="h-10 w-px bg-gray-300 mx-6"></div>
--- src/routes/_layout/tasks/index.tsx
+++ src/routes/_layout/tasks/index.tsx
@@ -1,4 +1,5 @@
-import Tasks from "@/components/task/Tasks";
+
+import Tasks from "@/components/Taskfiles/Tasks";
 import { createFileRoute } from "@tanstack/react-router";
 
 export const Route = createFileRoute("/_layout/tasks/")({
