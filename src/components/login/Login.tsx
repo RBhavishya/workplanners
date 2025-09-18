6146b171c0b0c5b6bb4b4adc4b94b14496e34800
hemanthoro <hemanthoro>
2025-09-18 15:46:34 +0530

    hc--update a email feild and password

--- src/components/login/Login.tsx
+++ src/components/login/Login.tsx
@@ -128,7 +128,7 @@ const LoginPage: React.FC = () => {
             <div>
               <label className="block text-sm mb-1">Email</label>
               <input
-                type="email"
+                id="email"
                 placeholder="Enter your email"
                 className="w-full px-4 py-2 border rounded-lg text-sm focus:ring focus:ring-indigo-200 outline-none"
                 value={email}
@@ -139,7 +139,7 @@ const LoginPage: React.FC = () => {
             <div>
               <label className="block text-sm mb-1">Password</label>
               <input
-                type="password"
+                id="password"
                 placeholder="Enter your password"
                 className="w-full px-4 py-2 border rounded-lg text-sm focus:ring focus:ring-indigo-200 outline-none"
                 value={password}
