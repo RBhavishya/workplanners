51548edbd86ad486f48b64ae7383261cfac8fdc7
hemanthoro <hemanthoro>
2025-09-18 15:52:54 +0530

    hc--update a currsor point in login slack

--- src/components/login/Login.tsx
+++ src/components/login/Login.tsx
@@ -102,11 +102,12 @@ const LoginPage: React.FC = () => {
           {/* Tabs */}
           <button
             onClick={() => slackAuthMutation.mutate()}
-            className="flex items-center justify-center gap-3 w-full px-6 py-3 border border-gray-300 rounded-lg mb-6"
+            className="flex items-center justify-center gap-3 w-full px-6 py-3 border border-gray-300 rounded-lg mb-6 cursor-pointer"
           >
             <img src={slackicon} alt="Slack" className="w-5 h-5" />
             <span className="text-gray-700 font-medium">
-              {slackAuthMutation.isPending || slackCallbackMutation.isPending ? (
+              {slackAuthMutation.isPending ||
+              slackCallbackMutation.isPending ? (
                 <span className="flex items-center gap-2">
                   <Loader2 className="w-4 h-4 animate-spin" /> Logging in...
                 </span>
